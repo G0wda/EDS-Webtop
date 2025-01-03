dd4b0355618c39cabfbac2dccb81bc099d8883c5
Void <NetVoid@gmail.com>
2025-01-03 13:01:17 +0530

    Added files

--- frontend/src/lib/components/Header.svelte
+++
@@ -1,5 +0,0 @@
-<div class="flex justify-center items-center justify-evenly  w-full h-[100px] bg-gradient-to-r from-slate-800 to-slate-900 ">
-    <h1>Edwin Dev Space</h1>
-    <a href="/">Ubuntu</a>
-
-</div>
